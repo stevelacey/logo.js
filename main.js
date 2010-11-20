fdb19a8e971c13c501dcc43fe720b0321a05fd92
Steve Lacey <steve@stevelacey.net>
2010-11-20 19:42:30 +0000

    Simplified.

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 var sys     = require("sys"),
     request = require("request"),
-    jsdom   = require("jsdom"),
+    $  = require("jquery"),
     uri     = 'http://www.stevelacey.net';
 
-request({uri:uri}, function (error, response, body) {
+request({uri:uri}, function (error, response, context) {
   if (!error && response.statusCode == 200) {
-    var window  = jsdom.jsdom(body).createWindow();
-    jsdom.jQueryify(window, "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js", function() {
-      sys.puts(uri + "/" + window.jQuery("#logo img").attr("src"));
-    });
+    console.log($("#logo img", context).attr('src'))
   }
-});
+})
