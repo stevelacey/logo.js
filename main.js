d3dd4ee4a4405eef856b1f758bfa003fd1687e48
Steve Lacey <steve@stevelacey.net>
2010-11-21 22:32:36 +0000

    Improved div regex and added support for OpenGraph images and iOS icons.

--- main.js
+++ main.js
@@ -10,26 +10,36 @@ function pathToLogo(logo) {
 
 request({uri:uri}, function (error, response, context) {
   if(!error && response.statusCode == 200) {
-    var imgs = ['#header img:first', '.header img:first', '#logo img', 'h1 img', '.logo img'];
-    var divs = ['#header', '.header', '.logo', '.header .logo', 'h1'];
-    var stylesheets = ['main', 'style', 'screen', 'global'];
+    var imgs = ['#header img:first', '.header img:first', '#logo img', 'h1 img', '.logo img', 'img#logo', 'img.logo']; // css
+    var divs = ['[#|.]header [#|.]logo', '[#|.]logo', '[#|.][^\\s]*logo[^\\s&^{]*', '[#|.]header', 'h1']; // regex
+    var stylesheets = ['main', 'style', 'screen', 'global']; // filenames
     
     // Try imgs
     var logo = $(imgs.join(','), context).attr('src');
     
     if(logo === undefined) {
+      // Check for an OpenGraph image
+      var logo = $('meta[property="og:image"]', context).attr('content');
+    }
+    
+    if(logo === undefined) {
+      // Check for an iOS icon
+      var logo = $('link[rel="apple-touch-icon-precomposed"], link[rel="apple-touch-icon"]', context).attr('href');
+    }
+    
+    if(logo === undefined) {
       // Try background-images
       for(var i in stylesheets) {
-        stylesheets[i] = 'link[rel="stylesheet"][href*="' + stylesheets[i] + '"]';
+        stylesheets[i] = 'link[rel="stylesheet"][href*="' + stylesheets[i] + '.css"]';
       }
 
       var stylesheet = $(stylesheets.join(','), context).attr('href');
-      
+       
       if(stylesheet !== undefined) {
         request({uri:url.resolve(uri, stylesheet)}, function (error, response, css) {
           if(!error && response.statusCode == 200) {
             for(var i in divs) {
-              var regex = new RegExp(divs[i] + "\\s*{[^}]*background-image:\\s*url\\s*\\(\\s*[\"|\']*([^\"&^\'&^)&^}]+)");
+              var regex = new RegExp(divs[i] + "\\s*{[^}]*background[^:]*:\\s*url\\s*\\(\\s*[\"|\']*([^\"&^\'&^)&^}]+)");
               var matches = css.match(regex);
               
               if(matches !== null) {
