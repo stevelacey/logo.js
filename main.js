ea2e986262a23d5ee8d9c0c035b644fbd70ea9b0
Steve Lacey <steve@stevelacey.net>
2010-11-21 21:18:31 +0000

    Further work improving quality of results, now checking primary stylesheet for probable selectors with background-image when no viable img's are found.

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 var sys     = require("sys"),
     request = require("request"),
-    $  = require("jquery"),
-    uri     = 'http://www.stevelacey.net';
+    url     = require("url"),
+    $       = require("jquery"),
+    uri     = 'http://' + process.argv[2] + '/';
+
+function pathToLogo(logo) {
+  console.log(logo !== undefined ? url.resolve(uri, logo) : false);
+}
 
 request({uri:uri}, function (error, response, context) {
   if (!error && response.statusCode == 200) {
-    console.log($("#logo img", context).attr('src'))
+    var imgs = ['#header img:first', '.header img:first', '#logo img', 'h1 img', '.logo img'];
+    var divs = ['#header', '.header', '.logo', '.header .logo', 'h1'];
+    var stylesheets = ['main', 'style', 'global'];
+    
+    // Try imgs
+    var logo = $(imgs.join(','), context).attr('src');
+    
+    if (logo === undefined) {
+      // Try background-images
+      for(var i in stylesheets) {
+        stylesheets[i] = 'link[rel="stylesheet"][href*="' + stylesheets[i] + '"]';
+      }
+
+      var stylesheet = url.resolve(uri, $(stylesheets.join(','), context).attr('href'));
+
+      request({uri:stylesheet}, function (error, response, css) {
+        if (!error && response.statusCode == 200) {
+          for(var i in divs) {
+            var regex = new RegExp(divs[i] + "\\s*{[^}]*background-image:\\s*url\\s*\\(\\s*[\"|\']*([^\"&^\'&^)&^}]+)");
+            var matches = css.match(regex);
+            
+            if(matches !== null) {
+              pathToLogo(matches[1]);
+              break;
+            }
+          }
+        }
+      })
+    } else {
+      pathToLogo(logo);
+    }
   }
 })
