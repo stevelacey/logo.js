45ae5885cbab444b13470c695718be039aad5596
Steve Lacey <steve@stevelacey.net>
2010-11-22 20:16:25 +0000

    Corrected indenting.

--- main.js
+++ main.js
@@ -30,65 +30,65 @@ if (uri == '') {
   respond(webresponse);
 } else {
 
-console.log(uri);
+  console.log(uri);
 
-request({uri:uri}, function (error, response, context) {
-  if(!error && response.statusCode == 200) {
-    var imgs = ['#header img:first', '.header img:first', '#logo img', 'h1 img', '.logo img', 'img#logo', 'img.logo', '#banner img:first']; // css
-    var divs = ['[#|.]header [#|.]logo', '[#|.]logo', '[#|.][^\\s]*logo[^\\s&^{]*', '[#|.]header', 'h1']; // regex
-    var stylesheets = ['main', 'style', 'screen', 'global']; // filenames
-    
-    // Try imgs
-    var logo = $(imgs.join(','), context).attr('src');
-    
-    if(logo === undefined) {
-      // Check for an OpenGraph image
-      var logo = $('meta[property="og:image"]', context).attr('content');
-    }
-    
-    if(logo === undefined) {
-      // Check for an iOS icon
-      var logo = $('link[rel="apple-touch-icon-precomposed"], link[rel="apple-touch-icon"]', context).attr('href');
-    }
-    
-    if(logo === undefined) {
-      // Try background-images
-      for(var i in stylesheets) {
-        stylesheets[i] = 'link[rel="stylesheet"][href*="' + stylesheets[i] + '.css"]';
+  request({uri:uri}, function (error, response, context) {
+    if(!error && response.statusCode == 200) {
+      var imgs = ['#header img:first', '.header img:first', '#logo img', 'h1 img', '.logo img', 'img#logo', 'img.logo', '#banner img:first']; // css
+      var divs = ['[#|.]header [#|.]logo', '[#|.]logo', '[#|.][^\\s]*logo[^\\s&^{]*', '[#|.]header', 'h1']; // regex
+      var stylesheets = ['main', 'style', 'screen', 'global']; // filenames
+
+      // Try imgs
+      var logo = $(imgs.join(','), context).attr('src');
+
+      if(logo === undefined) {
+        // Check for an OpenGraph image
+        var logo = $('meta[property="og:image"]', context).attr('content');
       }
 
-      var stylesheet = $(stylesheets.join(','), context).attr('href');
+      if(logo === undefined) {
+        // Check for an iOS icon
+        var logo = $('link[rel="apple-touch-icon-precomposed"], link[rel="apple-touch-icon"]', context).attr('href');
+      }
 
-      if(stylesheet !== undefined) {
-        console.log(url.resolve(uri, stylesheet));
-        request({uri:url.resolve(uri, stylesheet)}, function (error, response, css) {
-          if(!error && response.statusCode == 200) {
-            for(var i in divs) {
-              var regex = new RegExp(divs[i] + "\\s*{[^}]*background[^:]*:\\s*url\\s*\\(\\s*[\"|\']*([^\"&^\'&^)&^}]+)");
-              var matches = css.match(regex);
-              if(matches !== null) {
-                var logo = matches[1];
-                break;
+      if(logo === undefined) {
+        // Try background-images
+        for(var i in stylesheets) {
+          stylesheets[i] = 'link[rel="stylesheet"][href*="' + stylesheets[i] + '.css"]';
+        }
+
+        var stylesheet = $(stylesheets.join(','), context).attr('href');
+
+        if(stylesheet !== undefined) {
+          console.log(url.resolve(uri, stylesheet));
+          request({uri:url.resolve(uri, stylesheet)}, function (error, response, css) {
+            if(!error && response.statusCode == 200) {
+              for(var i in divs) {
+                var regex = new RegExp(divs[i] + "\\s*{[^}]*background[^:]*:\\s*url\\s*\\(\\s*[\"|\']*([^\"&^\'&^)&^}]+)");
+                var matches = css.match(regex);
+                if(matches !== null) {
+                  var logo = matches[1];
+                  break;
+                }
+              }
+
+              if(logo !== undefined) {
+                respond(webresponse, logo);
+              } else {
+                respond(webresponse);
               }
             }
-            
-            if(logo !== undefined) {
-              respond(webresponse, logo);
-            } else {
-              respond(webresponse);
-            }
-          }
-        })
+          })
+        } else {
+          respond(webresponse);
+        }
       } else {
-        respond(webresponse);
+        respond(webresponse, logo);
       }
     } else {
-      respond(webresponse, logo);
+      respond(webresponse);
     }
-  } else {
-    respond(webresponse);
-  }
-})
+  })
 
 }
 
